aa72f506c3b0dedd95ae355ea34d4f48a8c662fd
Sun1ive <sunliveua@gmail.com>
2018-02-10 22:15:03 +0200

    upd

---
+++ src/config/firebaseConfig.ts
@@ -0,0 +1,8 @@
+export default {
+  apiKey: 'AIzaSyDCxHsSoNNG2un3PQmZEnubatCAfdds3jU',
+  authDomain: 'dressing-room-f35be.firebaseapp.com',
+  databaseURL: 'https://dressing-room-f35be.firebaseio.com',
+  projectId: 'dressing-room-f35be',
+  storageBucket: 'dressing-room-f35be.appspot.com',
+  messagingSenderId: '602558021113',
+};
